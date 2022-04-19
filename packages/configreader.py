f452f5c367f56cc8c7489070e6e6362259e318ca
Adsicmes <1483492332@qq.com>
2022-04-19 10:09:03 +0800

    更改配置文件读取逻辑

--- packages/configreader.py
+++ packages/configreader.py
@@ -3,7 +3,4 @@ import json
 
 def config_reader(content_to_return: str) -> dict:
     j = json.load(open("config.json", 'r'))
-    match content_to_return:
-        case "osu_client": return j["osu_client"]
-        case "mysql": return j["mysql"]
-        case "osu_account": return j["osu_account"]
+    return j[content_to_return]
