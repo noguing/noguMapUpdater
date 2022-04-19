9fda232a66ef79dd0422fe1e4e3d853b13ecd729
Adsicmes <1483492332@qq.com>
2022-04-19 10:10:21 +0800

    添加保存方式 当前已有保存方式:1.本地json;2.mongodb数据库

--- packages/map_sid_getter_playwright.py
+++ packages/map_sid_getter_playwright.py
@@ -1,20 +1,14 @@
 import re
+import pymongo
 
-from playwright.sync_api import Playwright, sync_playwright, expect
+from playwright.sync_api import Playwright
 from packages.configreader import config_reader
 from time import strftime, localtime
 from loguru import logger
 from json import dumps as jsondumps
 
-PROXY_HTTP = "http://127.0.0.1:54433"
-# PROXY_SOCKS5 = "socks5://127.0.0.1:51837"
 
-browserLaunchOptionDict = {
-    "headless": True,
-    "proxy": {
-        "server": PROXY_HTTP,
-    }
-}
+CONFIG = config_reader("get_map")
 
 
 # 生成器，负责分文件进行保存
@@ -52,7 +46,7 @@ def file_reset():
 FR = file_reset()  # 初始化生成器
 
 
-def sid_response(response):
+def save_to_file(response):
     try:  # 报错则不是json，不添加(铺面搜索界面的json只有铺面)
         resj = response.json()  # 放在前边，用于检测是否为json，不是直接报错退出等待下一次调用
         try:
@@ -66,11 +60,70 @@ def sid_response(response):
         pass
 
 
+def connect_to_mongodb():
+    try:
+        logger.info("尝试登入mongodb")
+        mongo_config = config_reader("mongodb")
+        if (mongo_config["user"] is None and mongo_config["password"] is None) or \
+           (mongo_config["user"] == "" and mongo_config["password"] == ""):
+            # 无密码连接
+            logger.info("config.json中没有填写用户名与密码，采用无密码连接")
+            mongo_client = pymongo.MongoClient(
+                f"mongodb://{mongo_config['host']}:{mongo_config['port']}/")
+        else:
+            logger.info("在config.json中检测到用户名与密码，采用无密码连接")
+            # 有密码连接
+            mongo_client = pymongo.MongoClient(
+                f"mongodb://{mongo_config['user']}:{mongo_config['password']}@{mongo_config['host']}:{mongo_config['port']}/")
+
+        logger.info(f"链接信息:\n{mongo_client.server_info()}")
+
+        logger.info(f"接入数据库{mongo_config['database']}")
+        mongo_db = mongo_client[mongo_config["database"]]
+        logger.info(f"接入表{mongo_config['collection']}")
+        mongo_collection = mongo_db[mongo_config["collection"]]
+    except Exception as e:
+        logger.error(f"接入失败\n{e}")
+        exit(0)
+    return mongo_collection
+
+
+if CONFIG["beatmapset_save_type"] == "mongodb":
+    MONGO = connect_to_mongodb()
+
+
+def save_to_mongodb(response):
+    try:
+        resj = response.json()
+        try:
+            logger.info("保存数据到mongodb")
+            MONGO.insert_many([i for i in resj["beatmapsets"]])
+        except Exception as e:
+            logger.error(e)
+    except Exception:
+        pass
+
+
+def sid_response(response):
+    match CONFIG["beatmapset_save_type"]:
+        case "file": save_to_file(response)
+        case "mongodb": save_to_mongodb(response)
+
+
 def run(playwright: Playwright) -> None:
+    browser_launch_option_dict = {
+        "headless": False
+    }
+
     logger.info("读取配置文件")
+    if CONFIG["proxy"]["server"] is None or CONFIG["proxy"]["server"] == "":
+        browser_launch_option_dict["proxy"] = {
+            "server": CONFIG["proxy"]["server"],
+        }
     config = config_reader("osu_account")
+
     logger.info("playwright: 打开浏览器")
-    browser = playwright.chromium.launch(**browserLaunchOptionDict)
+    browser = playwright.chromium.launch(**browser_launch_option_dict)
     context = browser.new_context()
 
     context.route("**/*.{png,jpg,jpeg}", lambda route: route.abort())
@@ -80,6 +133,9 @@ def run(playwright: Playwright) -> None:
 
     logger.info("playwright: 跳转至osu.ppy.sh/beatmapsets")
     page.goto("https://osu.ppy.sh/beatmapsets", timeout=3000000)
+
+    # # 关闭图片加载
+    # page.evaluate("document.querySelector(\".beatmapset-panel\").innerHTML = \"<span>F</span>\"")
 
     # Click .avatar.avatar--nav2
     page.locator(".avatar.avatar--nav2").click()
@@ -130,6 +186,6 @@ def run(playwright: Playwright) -> None:
         page.evaluate("window.scrollTo(0,document.body.scrollHeight);")
 
     # ---------------------
-    context.close()
-    browser.close()
+    # context.close()
+    # browser.close()
 
